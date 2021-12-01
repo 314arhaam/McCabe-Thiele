48c3adaf1f56ed49134351023d32c0a045ce3c4f
Parhaam Abbasi <prmbas@gmail.com>
2021-12-02 00:50:46 +0330

    McCabe-Thiele method, functional ver. Haskell
    Should be updated, solves only for `q=1` cases

---
+++ haskell/distill.hs
@@ -0,0 +1,32 @@
+-- McCabe-Thiele method q = 1
+operationLine :: Float -> Float -> Float -> Float -> (Float -> Float)
+operationLine xb xd zf r = \x -> case () of
+                               _| x >  xm -> a1 * x + b1 -- stripping
+                                | x <= xm -> a2 * x + b2 -- rectifying
+                                where
+                                    xm = zf
+                                    a1 = r / (r + 1)
+                                    b1 = xd / (r + 1)
+                                    a2 = (a1 * xm + b1 - xb) / (xm - xb)
+                                    b2 = xb - a2 * xb
+
+
+distil :: Float -> (Float -> Float) -> (Float -> Float) -> [Float]
+distil x oLine invEquil | x_ >  oLine x_ = [x]
+                        | x_ <= oLine x_ = x : distil (oLine x_) oLine invEquil
+                        where
+                            x_ = invEquil x
+
+
+main :: IO ()
+main = do
+    let xb  = 0.1 -- buttoms molar concentration
+        xd  = 0.9 -- tops molar concentration
+        zf  = 0.5 -- feed concentration
+        rr  = 0.6 -- reflux ratio
+        al  = 6.0 -- relative volatility (alpha)
+    let inverseEquilibrium = \y -> y / (al - (al - 1) * y) -- invEq
+    let operatingLinesFun = operationLine xb xd zf rr -- apply McCabe
+    let trays = distil xd operatingLinesFun inverseEquilibrium
+    print trays
+    print("Number of Trays", length trays - 1)
